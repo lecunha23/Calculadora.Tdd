5fe7bf7ecc7dd487e623601654eecdf2118da20f
Leandro Cunha <103938030+lecunha23@users.noreply.github.com>
2023-05-16 21:00:32 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Tdd
+{
+    public class Calculadora
+    {
+        private int _limineMinimo;
+
+        public Calculadora(int limineMinimo = 0)
+        {
+            _limineMinimo = limineMinimo;
+        }
+
+        public int Somar(int a, int b)
+        {
+            if (( a < _limineMinimo) || ( b < _limineMinimo))
+            {
+                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite minimo.");
+            }
+            return a + b;
+        }
+
+        public int Subtrair(int a, int b) 
+        {
+            if ((a < _limineMinimo) || (b < _limineMinimo))
+            {
+                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite minimo.");
+            }
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Dividir(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    
+    }
+}
